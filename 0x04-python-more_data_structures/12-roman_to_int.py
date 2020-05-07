072d4d356406801d4fa72c5ea4638a98f25d9e2c
achrefbs <siachrefboularess@gmail.com>
2020-05-07 21:12:22 +0000

    ex12 update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -1,10 +1,12 @@
 #!/usr/bin/python3
 def roman_to_int(roman_string):
+    if len(roman_string) == 0 or not isinstance(roman_string, str):
+        return 0
     n = 0
     rom_to_int = {
         'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000
         }
-    if len(roman_string) == 0 or not isinstance(roman_string, str):
+    if not roman_string:
         return 0
     for i in roman_string:
         for j in rom_to_int.keys():
