76d8d92776ac1171e30db2fb6c52aace5b9b8637
achrefbs <siachrefboularess@gmail.com>
2020-05-18 17:41:39 +0000

    ex2 quick Fix 2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -10,8 +10,6 @@ def safe_print_list_integers(my_list=[], x=0):
         except (ValueError, TypeError):
             i += 1
             continue
-        except:
-            break
 
     print()
     return c
