80e51461e0b0e41f5a94e88675763accf6289109
achrefbs <siachrefboularess@gmail.com>
2020-05-18 16:07:46 +0000

    ex2

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+def safe_print_list_integers(my_list=[], x=0):
+    i = 0
+    c = 0
+    while i < x:
+        try:
+            print("{:d}".format(my_list[i]), end="")
+            c += 1
+            i += 1
+        except ValueError:
+            i += 1
+            continue
+        except:
+            break
+
+    print()
+    return c
