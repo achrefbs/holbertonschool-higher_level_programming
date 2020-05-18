d6b3db5fa18b84b10696dcd6c43a0614a01be447
achrefbs <siachrefboularess@gmail.com>
2020-05-18 17:36:21 +0000

    ex7 update

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -7,5 +7,5 @@ def safe_print_integer_err(value):
         print("{:d}".format(value))
         return True
     except ValueError as err:
-        sys.stderr.write("Exception: " + str(err) + "\n")
+        print('Exception: {}'.format(err), file=sys.stderr)
         return False
