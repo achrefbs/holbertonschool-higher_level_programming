d3a374d4b7ed139bbbce0ed49329ecdfd58f32da
achrefbs <siachrefboularess@gmail.com>
2020-05-19 15:18:08 +0000

    ex3

--- 0x06-python-classes/3-square.py
+++ 0x06-python-classes/3-square.py
@@ -1,1 +1,17 @@
 #!/usr/bin/python3
+"""a class Square that defines a square"""
+
+
+class Square:
+    """a class Square that defines a square"""
+    def __init__(self, size=0):
+        """instantiation with optional size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+
+    def area(self):
+        """returns the current square area"""
+        return self.__size * self.__size
