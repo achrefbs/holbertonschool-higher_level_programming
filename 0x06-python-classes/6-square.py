2572fa7fa209132ac14bb1daa7f9e14eef2fceac
achrefbs <siachrefboularess@gmail.com>
2020-05-19 16:16:04 +0000

    ex6

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -1,1 +1,61 @@
 #!/usr/bin/python3
+"""a class Square that defines a square"""
+
+
+class Square:
+    """a class Square that defines a square"""
+    def __init__(self, size=0, position=(0, 0)):
+        """instantiation with optional size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        if len(position) != 2:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(position[0]) != int or position[0] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        if type(position[1]) != int or position[1] < 0:
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__size = size
+        self.__position = position
+
+    def area(self):
+        """returns the current square area"""
+        return self.__size * self.__size
+
+    @property
+    def size(self):
+        """getter"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """setter"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        if value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    @property
+    def position(self):
+        """getter"""
+        return self.__position
+
+    @position.setter
+    def position(self, value):
+        """setter"""
+        if len(value) != 2 or type(value(0)) != int or type(value[1] != int):
+            raise TypeError("position must be a tuple of 2 positive integers")
+        self.__position = value
+
+    def my_print(self):
+        """that prints in stdout the square with the character #"""
+        if self.size == 0:
+            print()
+        for i in range(self.size):
+            for c in range(self.position[0]):
+                print(end=' ')
+            for j in range(self.size):
+                print("#", end='')
+            print()
