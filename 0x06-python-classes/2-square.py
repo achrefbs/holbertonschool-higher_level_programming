fe1e12c6a08330023d013d70117f0f77c919d870
achrefbs <siachrefboularess@gmail.com>
2020-05-19 15:13:03 +0000

    ex2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,1 +1,13 @@
 #!/usr/bin/python3
+"""a class Square that defines a square"""
+
+
+class Square:
+    """a class Square that defines a square"""
+    def __init__(self, size=0):
+        """instantiation with optional size"""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
