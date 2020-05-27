11378b7be6ac01711c641fb8de25ede8aeb1161f
achrefbs <siachrefboularess@gmail.com>
2020-05-27 12:22:26 +0000

    ex30

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,8 +1,4 @@
-#!/usr/bin/python3
-class magic_string: 
-    n = 0
-    def __str__(self):
-        magic_string.n += 1 ; return "holberton" * magic_string.n
-
-for i in range(10):
-    print(magic_string())
+def magic_string():
+    global i
+    if i != 0:
+        return "Holberton, " * (i - 1) + "Holberton"
