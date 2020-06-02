1929d87b38211f5d2c92e842fcc66ed024ce3fd6
achrefbs <siachrefboularess@gmail.com>
2020-06-02 18:11:19 +0000

    ex4

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""object is an instance of a class that inherited"""
+
+
+def inherits_from(obj, a_class):
+    """checks if an object is an instance of a class that inherited"""
+    return issubclass(obj, a_class)
