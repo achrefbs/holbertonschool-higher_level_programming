eba37c5421e97addd262f66beb6a9097cde79e44
achrefbs <siachrefboularess@gmail.com>
2020-06-03 21:59:53 +0000

    ex11

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""a class Student that defines a student"""
+
+
+class Student:
+    """a class Student that defines a student"""
+    def __init__(self, first_name, last_name, age):
+        """init"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def class_to_json(obj):
+        """returns the dictionary description with simple data structure"""
+        return(obj.__dict__)
