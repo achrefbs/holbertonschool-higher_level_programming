fd97e162c33d9fb53cc6edb88aeb31cda2bba1be
achrefbs <siachrefboularess@gmail.com>
2020-06-03 14:24:32 +0000

    ex3

--- 0x0B-python-input_output/3-write_file.py
+++ 0x0B-python-input_output/3-write_file.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""writes a string to a text file"""
+
+
+def write_file(filename="", text=""):
+    """
+    writes a string to a text file
+    return: the number of characters written
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        f.write(text)
+    return len(text)
