116110e2345df46c3768a607a067c6b83346277d
achrefbs <siachrefboularess@gmail.com>
2020-06-03 21:09:28 +0000

    ex8

--- 0x0B-python-input_output/8-load_from_json_file.py
+++ 0x0B-python-input_output/8-load_from_json_file.py
@@ -1,1 +1,9 @@
 #!/usr/bin/python3
+"""a function that creates an Object from a “JSON file”"""
+import json
+
+
+def load_from_json_file(filename):
+    """a function that creates an Object from a “JSON file”:"""
+    with open(filename, encoding="utf-8") as f:
+        return json.load(f)
