b6f06ccd37d85127843281afb09edec443f29418
achrefbs <siachrefboularess@gmail.com>
2020-06-03 14:19:25 +0000

    ex2

--- 0x0B-python-input_output/2-read_lines.py
+++ 0x0B-python-input_output/2-read_lines.py
@@ -1,1 +1,15 @@
 #!/usr/bin/python3
+"""reads n lines of a text file (UTF8) and prints it to stdout"""
+
+
+def read_lines(filename="", nb_lines=0):
+    """reads n lines of a text file (UTF8) and prints it to stdout"""
+    with open(filename, encoding="utf-8") as f:
+        if nb_lines <= 0:
+            print(f.read())
+        else:
+            line = f.readline()
+            while line and nb_lines > 0:
+                print(line, end="")
+                line = f.readline()
+                nb_lines -= 1
