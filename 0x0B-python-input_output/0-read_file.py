871b96e011e8525931f7ca40f7bb44c3aa650ef6
achrefbs <siachrefboularess@gmail.com>
2020-06-03 20:51:04 +0000

    ex0 update

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,5 +6,3 @@ def read_file(filename=""):
     """a function that reads a text file (UTF8) and prints it to stdout"""
     with open(filename, encoding="utf-8") as f:
         print(f.read(), end="")
-
-read_file("my_file_0.txt")
