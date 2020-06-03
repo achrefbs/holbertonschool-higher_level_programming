e007586c921004859662bbba8f257e06eb734b06
achrefbs <siachrefboularess@gmail.com>
2020-06-03 14:30:12 +0000

    ex4

--- 0x0B-python-input_output/4-append_write.py
+++ 0x0B-python-input_output/4-append_write.py
@@ -1,1 +1,12 @@
 #!/usr/bin/python3
+"""function that appends a string at the end of a text file"""
+
+
+def append_write(filename="", text=""):
+    """
+    function that appends a string at the end of a text file
+    return: the number of characters added
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        f.write(text)
+    return len(text)
