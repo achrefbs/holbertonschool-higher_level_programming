2fb6e3f58ebcd431ee8556ad1cba250aa2308fa2
achrefbs <siachrefboularess@gmail.com>
2020-06-09 15:49:01 +0000

    added grom_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -31,3 +31,9 @@ class Base:
                 for obj in list_objs:
                     l.append(obj.to_dictionary())
             f.write(cls.to_json_string(l))
+
+    @staticmethod
+    def from_json_string(json_string):
+        if not json_string:
+            return []
+        return json.loads(json_string)
