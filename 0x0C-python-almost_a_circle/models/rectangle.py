5d71e654b33ec7aad8ebedaf3a000066a18f93f1
achrefbs <siachrefboularess@gmail.com>
2020-06-09 11:52:59 +0000

    overriding __str__

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -79,3 +79,8 @@ class Rectangle(Base):
             for j in range(self.width):
                 print('#', end='')
             print()
+
+    def __str__(self):
+        return ('[{}] ({}) {}/{} - {}/{}'
+                .format(type(self).__name__, self.id, self.x, self.y,
+                        self.width, self.height))
