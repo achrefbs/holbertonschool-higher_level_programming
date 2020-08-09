d5df271224d1dc5c813344266c8b75cb9093861d
achrefbs <siachrefboularess@gmail.com>
2020-08-09 12:04:59 +0100

    task 3

---
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python3
+"""
+a script that takes in arguments and displays all values
+in the states table of hbtn_0e_0_usa where name matches
+the argument.But this time, write one that is safe
+from MySQL injections!
+"""
+import MySQLdb
+import sys
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    database = sys.argv[3]
+    name = sys.argv[4]
+
+    db = MySQLdb.connect(
+            host="localhost",
+            port=3306,
+            user=username,
+            passwd=password,
+            db=database
+            )
+
+    cursor = db.cursor()
+    cursor.execute("""SELECT * FROM states WHERE BINARY name = %s
+                    ORDER BY id""", (name,))
+    rows = cursor.fetchall()
+    for row in rows:
+        print(row)
+    cursor.close()
+    db.close()
