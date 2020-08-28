3736ae49fdd7c31ba570ae8f1652dbc38dbc007e
achrefbs <siachrefboularess@gmail.com>
2020-08-28 11:55:31 +0100

    task 0

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""a Python script that fetches https://intranet.hbtn.io/status"""
+
+import urllib.request
+with urllib.request.urlopen('https://intranet.hbtn.io/status') as r:
+    html = r.read()
+    print('Body response:')
+    print("\t- type: {}".format(type(html)))
+    print("\t- content: {}".format(html))
+    print("\t- utf8 content: {}".format(html.decode('utf-8')))
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,2 @@
+# 0. What's my status? #0 :
+- a Python script that fetches https://intranet.hbtn.io/status
