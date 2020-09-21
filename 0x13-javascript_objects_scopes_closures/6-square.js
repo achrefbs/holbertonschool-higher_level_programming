601ad59efac9b32dd80e74e315a05696c94441a0
achref <achrefbs>
2020-09-21 18:51:01 +0100

    task 6

--- 0x13-javascript_objects_scopes_closures/6-square.js
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -2,7 +2,7 @@
 const Square1 = require('./5-square');
 module.exports = class Square extends Square1 {
   charPrint (c) {
-    if (c == undefined) {
+    if (c === undefined) {
       this.print();
     } else {
       for (let i = 0; i < this.height; i++) {
