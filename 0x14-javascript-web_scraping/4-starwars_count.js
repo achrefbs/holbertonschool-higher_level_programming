06d64f6fcb9a1bdcf76289a236f1977bdc6159b8
achref <achrefbs>
2020-09-22 17:42:27 +0100

    task 4 update

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -1,10 +1,16 @@
 #!/usr/bin/node
 const request = require('request');
 
-request('https://swapi-api.hbtn.io/api/people/18/', function (err, res, body) {
+request(process.argv[2], function (err, res, body) {
+  let c = 0;
   if (err) {
     console.log(err);
   }
   const json = JSON.parse(body);
-  console.log(json.films.length);
+  for (let i = 0; json.results[i] !== undefined; i++) {
+    if (json.results[i].characters.includes('https://swapi-api.hbtn.io/api/people/18/')) {
+      c++;
+    }
+  }
+  console.log(c);
 });
